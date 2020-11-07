61f103f0b348dee7d0e70e6f8aafe9a8dfb83efb
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 12:55:34 +0500

    Test Timer Btton with another method

--- src/components/TimerButton/TimerButton.spec.tsx
+++ src/components/TimerButton/TimerButton.spec.tsx
@@ -1,1 +1,20 @@
-import React from "react";
+import React from "react"
+import { shallow } from "enzyme"
+import TimerButton from "./TimerButton"
+
+describe("TimerButton", () => {
+  let container
+
+  beforeEach(() => {
+    container = shallow(
+      <TimerButton
+        buttonAction={jest.fn()}
+        buttonValue={""}
+      />
+    )
+  })
+
+  it("should render a <div />", () => {
+    expect(container.find("div").length).toBeGreaterThanOrEqual(1)
+  })
+})

a174114aeb6e6d16b06115fb8d32204c72d36951
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 12:15:01 +0500

    Test timer with another way

--- src/components/Timer/Timer.spec.tsx
+++ src/components/Timer/Timer.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import Timer from './Timer'
 describe('App components render', () => {
-  let container = shallow(<App />);
-  it('should render a container' , () => {
+  let container = shallow(<Timer />);
+  it('should render a timer' , () => {
     expect(container.find('div').length).toEqual(1)
     }) ;
 });

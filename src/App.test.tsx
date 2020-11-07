3265412ac3ba0139da55a68e67881a1562944293
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 11:10:31 +0500

    Run first Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the component', () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
 });

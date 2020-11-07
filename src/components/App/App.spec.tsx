539c96e0cf8d957a9194bbd5fc73df1961a3d570
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 12:20:34 +0500

    Give style to container

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Timer from './../Timer/Timer'
 describe('App components render', () => {
   let container = shallow(<App />);
-  it('should render a container' , () => {
+  it('should render a div' , () => {
     expect(container.find('div').length).toEqual(1)
     }) ;
+    it('should render a timer in container' , () => {
+      expect(container.containsMatchingElement(<Timer />)).toEqual(true)
+      }) 
 });

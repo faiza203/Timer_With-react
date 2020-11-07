820444a1f131f4cdb91e9925d0a565eec8e70d73
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 11:32:46 +0500

    Remove un needed data

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-test('renders the component', () => {
-  const component = shallow(<App />);
-  expect(component).toMatchSnapshot();
-});

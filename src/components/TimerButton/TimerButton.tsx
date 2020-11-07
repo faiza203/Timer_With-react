69dcb618645ea368b2778bcf7e869749d27a561e
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 13:03:29 +0500

    Test Buttons

--- src/components/TimerButton/TimerButton.tsx
+++ src/components/TimerButton/TimerButton.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TimerButton:any = ({ buttonAction, buttonValue }) => (
-  <div className="button-container" />
+const TimerButton: any = ({ buttonAction, buttonValue }) => (
+    <div className="button-container" />
 );
 
 TimerButton.propTypes = {
-  buttonAction: PropTypes.func.isRequired,
-  buttonValue: PropTypes.string.isRequired,
+    buttonAction: PropTypes.func.isRequired,
+    buttonValue: PropTypes.string.isRequired,
 };
 
 export default TimerButton;

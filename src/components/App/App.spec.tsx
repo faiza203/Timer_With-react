91ac791a57a5d613ff788b7c013864d911b1267a
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 12:00:30 +0500

    Create Timer folder in src/components

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-test('renders the component', () => {
-  const component = shallow(<App />);
-  expect(component).toMatchSnapshot();
+describe('App components render', () => {
+  it('should render a div' , () => {
+  let container = shallow(<App />);
+  expect(container.find('div').length).toEqual(1)
+  }) 
 });

00fc882f89a9678f9b59db8524c64fa8d2f02f66
faiza203 <fiazfaiza981@gmail.com>
2020-11-07 12:33:35 +0500

    Give margin and padding

--- src/components/Timer/Timer.spec.tsx
+++ src/components/Timer/Timer.spec.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Timer from './Timer'
 describe('App components render', () => {
-  let container = shallow(<Timer />);
-  it('should render a timer' , () => {
-    expect(container.find('div').length).toEqual(1)
-    }) ;
+    let container = shallow(<Timer />);
+    it('should render a timer', () => {
+            expect(container.find("div").length).toBeGreaterThanOrEqual(1)
+    });
 });
